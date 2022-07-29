4008b9f7678dda1f22c75667ece18b674eef83d8
kelemii <98933496+kelemii@users.noreply.github.com>
2022-07-29 13:44:47 -0700

    a lot of frontend changes

--- client/app.js
+++ client/app.js
@@ -1,4 +1,22 @@
+const $bros = $('.brosbtn');
+const $results = $('.results');
 
-$.get("/bros", (res) => {
-    console.log(res)
-})
+
+
+// for reference
+// const $container = $(".container");
+// const $loadTweets = $(".loadTweets");
+// const $userTweets = $(".userTweets");
+// const $tweedle = $(".tweedle");
+
+// $tweedle.click(() => {
+//   let username = prompt('What name do you go by?');
+//   let tweedleText = prompt('Type your message here.');
+// });
+$bros.click(() => {
+    $.get("/bros", (res) => {
+        $results.text(JSON.stringify(res));
+        console.log(res);
+    })
+});
+
